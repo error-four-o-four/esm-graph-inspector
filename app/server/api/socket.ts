a99a01208e1d11617ba7670e1e197a6d0d916f44
error-four-o-four <jens.bachmayer@gmx.de>
2025-08-17 16:38:40 +0200

    fix: minor
    * lints `.vscode/settings.json`
    * changes `app/server/api/socket.ts`
    	to handle development env

--- app/server/api/socket.ts
+++ app/server/api/socket.ts
@@ -3,6 +3,9 @@ import socketHandler from '~~/src/handler/socket.js';
 if (process.env.NODE_ENV === 'development') {
   (async () => {
     const { initialize } = await import('~~/src/data.js');
+    // @todo dev
+    // initialize({ entry: './nope.ts' });
+    // initialize({ entry: './_dummy/index.ts' });
     initialize({ entry: './src/index.ts' });
   })();
 }
